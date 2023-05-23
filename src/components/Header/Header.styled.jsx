e2228c7db7690613fdacd601b7e57622c1a4f514
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 00:38:19 +0300

    Create Header.styled.jsx

---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const StyledHeader = styled.header`
+  padding: 20px 30px;
+  box-shadow: 0px 4px 4px #d3d3d3;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 500;
+  transition: color 0.2s ease-in-out;
+  color: grey;
+
+  &:not(:last-child) {
+    margin-right: 30px;
+  }
+
+  &.active,
+  &:focus {
+    color: teal;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
