2ff27771e119a29fd7e897167cee7435c39400be
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-23 19:20:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/movies" element={<Movies />} />
+
+    <Route path="/movies/:movieId" element={<MovieDetails />} />
   </Routes>
 );
 
