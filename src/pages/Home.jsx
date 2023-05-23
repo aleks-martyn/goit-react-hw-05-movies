2c392bc326cef5708f7125700c0e51fbe4f325ce
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-23 23:39:01 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,3 @@
-import Header from 'components/Header/Header';
-
-const Home = () => {
-  return (
-    <>
-      <Header />
-      <div>Home</div>
-    </>
-  );
-};
+const Home = () => <div>Home</div>;
 
 export default Home;
