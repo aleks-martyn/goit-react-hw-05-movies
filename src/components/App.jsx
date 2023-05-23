b85d09c0f6d7ebb5a89f011ee8b8fdad915df11d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-23 23:39:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,17 @@ import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
 import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
+import Layout from './Layout/Layout';
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/movies" element={<Movies />} />
-
-    <Route path="/movies/:movieId" element={<MovieDetails />}>
-      
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="movies" element={<Movies />} />
+      <Route path="movies/:movieId" element={<MovieDetails />}>
+        <Route path="cast" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
     </Route>
   </Routes>
 );
