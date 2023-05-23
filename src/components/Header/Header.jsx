ed557dc5326e0903d923eebdc35ef249c855026b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-23 18:46:04 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
-const Header = () => {
+import { NavLink } from 'react-router-dom';
 
-}
+const Header = () => (
+  <header>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+    </nav>
+  </header>
+);
 
 export default Header;
