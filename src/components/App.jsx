761d4d2f1b61744814b60291a0fb344902899335
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-23 17:58:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Routes, Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/movies" element={<Movies />} />
+  </Routes>
+);
+
+export default App;
