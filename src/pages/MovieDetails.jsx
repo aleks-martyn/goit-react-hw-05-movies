3cfd610390e10c638c2ac8dea0ba5ca50d19378b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 17:04:06 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,14 +1,37 @@
-import { useState, useEffect } from "react";
-import { useParams, Outlet, useLocation, Link } from 'react-router-dom';
-import { fetchMovieById } from "services/api";
-import MovieCard from "components/MovieCard/MovieCard";
+import { useState, useEffect } from 'react';
+import { useParams, useLocation, Link } from 'react-router-dom';
+import { fetchMovieById } from 'services/api';
+import MovieCard from 'components/MovieCard/MovieCard';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const location = useLocation();
   const [selectedMovie, setSelectedMovie] = useState({});
-  
-  return <div>MovieDetails</div>;
+
+  useEffect(() => {
+    const fetchSelectedMovie = async movieId => {
+      try {
+        const movieData = await fetchMovieById(movieId);
+        setSelectedMovie(movieData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSelectedMovie(movieId);
+  }, [movieId]);
+
+  return (
+    <main>
+      <div>
+        <Link to={location?.state?.from ?? '/'}>
+          <button type='button'>Go back</button>
+        </Link>
+
+        <MovieCard movie={selectedMovie} />
+      </div>
+    </main>
+  );
 };
 
 export default MovieDetails;
