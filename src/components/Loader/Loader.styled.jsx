dbeff06e21c4e085c6b2ffe4bcd96a0d36be26de
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 14:27:23 +0300

    Create Loader.styled.jsx

---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export default Wrap;
