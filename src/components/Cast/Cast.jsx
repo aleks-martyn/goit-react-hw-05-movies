4bd9a075e0a6ddcdb34da0fb80be01441965d760
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 23:39:03 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -7,7 +7,7 @@ const Cast = () => {
   const [cast, setCast] = useState([]);
 
   useEffect(() => {
-    const fetchCast = async () => {
+    const fetchCast = async (movieId) => {
       try {
         const { cast } = await fetchMovieCast(movieId);
         setCast(cast);
@@ -16,15 +16,31 @@ const Cast = () => {
       }
     };
 
-    fetchCast();
+    fetchCast(movieId);
   }, [movieId]);
-
+ console.log(cast)
   return (
     <div>
       <h3>Cast</h3>
       <ul>
         {cast.map(actor => (
-          <li></li>
+          <li key={actor.id}>
+            {actor.profile_path ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                alt={`${actor.name} profile`}
+              />
+            ) : (
+              <img
+                src={`https://via.placeholder.com/200x300?text=No+Image`}
+                alt={`${actor.name} profile`}
+              />
+            )}
+            <div>
+              <h3>{actor.name}</h3>
+              <p></p>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
