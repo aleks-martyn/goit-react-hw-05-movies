3d9941fc99b81deb20149a29253c57838a8cd475
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 16:06:59 +0300

    Update MovieCard.styled.jsx

--- src/components/MovieCard/MovieCard.styled.jsx
+++ src/components/MovieCard/MovieCard.styled.jsx
@@ -1,2 +1,22 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
+export const StyledLink = styled(Link)`
+  font-size: 16px;
+  margin-right: 5px;
+  padding: 5px 15px;
+  border: 1px solid #0d57aa;
+  border-radius: 6px;
+  background-color: #b5d1f2;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  color: #0d57aa;
+
+  &:hover,
+  &:focus {
+    background-color: #0d57aa;
+    color: #ffffff;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  }
+`;
