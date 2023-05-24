cf19fbbbdab7dc5c0054506230e32967adc28c17
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 12:43:18 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
-const Home = () => <div>Home</div>;
+import { useState, useEffect } from 'react';
+import { fetchTrendMovies } from 'services/api';
+import MovieList from 'components/MovieList/MovieList';
+
+const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const { results } = await fetchTrendMovies();
+        setTrendingMovies(results);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return <MovieList trendingMovies={trendingMovies} />;
+};
 
 export default Home;
