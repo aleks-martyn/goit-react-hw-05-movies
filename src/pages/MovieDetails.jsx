5ff49723b58a83f62d6b024dad8c913940ef01a8
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 16:11:58 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams, Outlet, useLocation, Link } from 'react-router-dom';
 import { fetchMovieById } from "services/api";
+import MovieCard from "components/MovieCard/MovieCard";
 
 const MovieDetails = () => {
+  const { movieId } = useParams();
+  const location = useLocation();
+  const [selectedMovie, setSelectedMovie] = useState({});
+  
   return <div>MovieDetails</div>;
 };
 
