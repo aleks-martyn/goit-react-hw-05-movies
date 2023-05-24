936b5d809cc5ab3619bc114e8765eb7fc196fc3f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 22:32:00 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCast } from 'services/api';
+
 const Cast = () => {
-  return <div>Cast</div>;
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    const fetchCast = async () => {
+      try {
+        const { cast } = await fetchMovieCast(movieId);
+        setCast(cast);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCast();
+  }, [movieId]);
+
+  return (
+    <div>
+      <h3>Cast</h3>
+      <ul>
+        {cast.map(actor => (
+          <li></li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Cast;
