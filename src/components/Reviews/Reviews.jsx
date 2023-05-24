6a7418023c9cd1b6d65e96acb4c403c11a99a42f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 23:38:18 +0300

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieReviews } from 'services/api';
+
 const Reviews = () => {
-  return <div>Reviews</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const { results } = await fetchMovieReviews(movieId);
+        setReviews(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchReviews();
+  }, [movieId]);
+
+  return (
+    <div>
+      <h3>Reviews</h3>
+      {reviews.length ? (
+        <ul>
+          {reviews.map(review => (
+            <li key={review.id}>
+              <h4>Author: {review.author}</h4>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>We don't have any reviews for this movie yet.</p>
+      )}
+    </div>
+  );
 };
 
 export default Reviews;
