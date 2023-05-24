baca4e8da19de12e5c64dc1c687c8a17522ba68b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 16:07:06 +0300

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Spinner from 'components/Loader/Loader';
+import { StyledLink } from './MovieCard.styled';
 
 const MovieCard = ({ movie }) => {
   const { title, release_date, poster_path, vote_average, overview, genres } =
@@ -20,12 +20,48 @@ const MovieCard = ({ movie }) => {
     ? `${(vote_average * 10).toFixed(0)}%`
     : 'Not rated yet';
 
-    return (
-        <>
-            <div>
-                <img src={posterUrl} alt={`${title}`}/>
+  return (
+    <>
+      <div>
+        <img src={posterUrl} alt={`${title}`} />
+        <div>
+          <h2>
+            {title ?? 'Unknown'} ({releaseYear})
+          </h2>
+          <p>User Score: {userScore}</p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          {genres && genres.length > 0 && (
+            <>
+              <h3>Genres</h3>
+              <p>{genres.map(genre => genre.name).join(', ')}</p>
+            </>
+          )}
         </div>
-        </>
+      </div>
+
+      <div>
+        <h3>Additional information</h3>
+        <ul>
+          <li>
+            <StyledLink
+              to="cast"
+              state={{ from: location?.state?.from ?? '/' }}
+            >
+              Cast
+            </StyledLink>
+          </li>
+          <li>
+            <StyledLink
+              to="reviews"
+              state={{ from: location?.state?.from ?? '/' }}
+            >
+              Reviews
+            </StyledLink>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
