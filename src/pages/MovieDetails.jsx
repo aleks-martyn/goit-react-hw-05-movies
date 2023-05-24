ed6487014708aa55dbedd70ff2b370e30f969fb8
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 14:54:51 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { useParams, Outlet, useLocation, Link } from 'react-router-dom';
+import { fetchMovieById } from "services/api";
+
 const MovieDetails = () => {
   return <div>MovieDetails</div>;
 };
