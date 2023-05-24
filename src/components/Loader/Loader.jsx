d7b1f86cce42aa982c1d0df8c9832a7e06a51d26
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 14:25:24 +0300

    Create Loader.jsx

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import { RotatingLines } from 'react-loader-spinner';
+import { Wrap } from './Loader.styled';
+
+const Spinner = () => (
+  <Wrap role="alert">
+    <RotatingLines
+      strokeColor="grey"
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="96"
+      visible={true}
+    />
+  </Wrap>
+);
+
+export default Spinner;
