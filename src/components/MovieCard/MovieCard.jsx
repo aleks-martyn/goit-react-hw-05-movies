7363e506983a656c23752bec24ecc7d8dd5ae93c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-24 15:33:34 +0300

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,4 +2,44 @@ import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Spinner from 'components/Loader/Loader';
 
-const MovieCard = ({ movie }) => { }
+const MovieCard = ({ movie }) => {
+  const { title, release_date, poster_path, vote_average, overview, genres } =
+    movie;
+  const location = useLocation();
+
+  const releaseDate = new Date(release_date);
+  const releaseYear = isNaN(releaseDate)
+    ? 'Unknown'
+    : releaseDate.getFullYear();
+
+  const posterUrl = poster_path
+    ? `https://image.tmdb.org/t/p/w400/${poster_path}`
+    : 'https://via.placeholder.com/400x600.png?text=Poster+Not+Available';
+
+  const userScore = vote_average
+    ? `${(vote_average * 10).toFixed(0)}%`
+    : 'Not rated yet';
+
+    return (
+        <>
+            <div>
+                <img src={posterUrl} alt={`${title}`}/>
+        </div>
+        </>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    vote_average: PropTypes.number,
+    overview: PropTypes.string,
+    genres: PropTypes.arrayOf(
+      PropTypes.shape({ name: PropTypes.string.isRequired })
+    ),
+  }).isRequired,
+};
+
+export default MovieCard;
