43c40c5180576e2f15b7865cc7baa9c48292fa1f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-25 12:04:45 +0300

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams, useLocation } from 'react-router-dom'; 
+import { fetchMovieByName } from 'services/api';
+
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  
   return <div>Movies</div>;
 };
 
