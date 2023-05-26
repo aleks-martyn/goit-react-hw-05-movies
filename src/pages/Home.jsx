5214e71412c8fe49c784819b32dd437148a1a035
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-26 18:06:32 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
   useEffect(() => {
     const fetchTrendingMovies = async () => {
       try {
-        setStatus('pending');
         const { results } = await fetchTrendMovies();
         setTrendingMovies(results);
         setStatus('resolved');
