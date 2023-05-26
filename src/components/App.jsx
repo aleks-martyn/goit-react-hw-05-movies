dd6c5648956a40dcce60de53554f73b3c4e2f20f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-26 23:19:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout';
 
 const Home = lazy(() => import('pages/Home'));
@@ -17,6 +17,7 @@ const App = () => (
         <Route path="cast" element={<Cast />} />
         <Route path="reviews" element={<Reviews />} />
       </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Route>
   </Routes>
 );
