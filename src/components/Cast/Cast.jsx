5b52a78f17edeeff75b16034b9a600dd85d2d799
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-26 18:06:15 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieCast } from 'services/api';
+import Spinner from 'components/Loader/Loader';
 import {
   Wrap,
   CastTitle,
@@ -14,51 +15,61 @@ import {
 const Cast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState([]);
+  const [status, setStatus] = useState('pending');
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchCast = async () => {
+    const fetchCast = async movieId => {
       try {
         const { cast } = await fetchMovieCast(movieId);
         setCast(cast);
+        setStatus('resolved');
       } catch (error) {
-        console.log(error);
+        setError(error);
+        setStatus('rejected');
       }
     };
 
-    fetchCast();
+    fetchCast(movieId);
   }, [movieId]);
 
   return (
-    <Wrap>
-      <CastTitle>Cast</CastTitle>
-      {cast.length ? (
-        <CastList>
-          {cast.map(({ id, profile_path, name, character }) => (
-            <CastListItem key={id}>
-              {profile_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${profile_path}`}
-                  alt={`${name} profile`}
-                />
-              ) : (
-                <img
-                  src={`https://via.placeholder.com/200x300?text=No+Image`}
-                  alt={`${name} profile`}
-                />
-              )}
-              <CastInfo>
-                <CastName>{name}</CastName>
-                <p>Character: {character}</p>
-              </CastInfo>
-            </CastListItem>
-          ))}
-        </CastList>
-      ) : (
-        <NoCastText>
-          We don't have any information about the cast yet.
-        </NoCastText>
+    <>
+      {status === 'pending' && <Spinner />}
+      {status === 'rejected' && <h3>{error.message}</h3>}
+      {status === 'resolved' && (
+        <Wrap>
+          <CastTitle>Cast</CastTitle>
+          {cast.length ? (
+            <CastList>
+              {cast.map(({ id, profile_path, name, character }) => (
+                <CastListItem key={id}>
+                  {profile_path ? (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w185${profile_path}`}
+                      alt={`${name}`}
+                    />
+                  ) : (
+                    <img
+                      src={`https://via.placeholder.com/185x278?text=No+Image`}
+                      alt={`${name}`}
+                    />
+                  )}
+                  <CastInfo>
+                    <CastName>{name}</CastName>
+                    <p>Character: {character}</p>
+                  </CastInfo>
+                </CastListItem>
+              ))}
+            </CastList>
+          ) : (
+            <NoCastText>
+              We don't have any information about the cast yet.
+            </NoCastText>
+          )}
+        </Wrap>
       )}
-    </Wrap>
+    </>
   );
 };
 
