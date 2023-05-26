ff596d8ea0c75490f26f78c2cbc4045a5e3b0cb2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-26 13:05:30 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Spinner from 'components/Loader/Loader';
 import Header from 'components/Header/Header';
 
 const Layout = () => (
   <>
     <Header />
-    <Outlet />
+    <Suspense fallback={<Spinner />}>
+      <Outlet />
+    </Suspense>
   </>
 );
 
