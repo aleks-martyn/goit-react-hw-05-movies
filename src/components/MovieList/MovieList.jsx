522f247b46fd05ccf5592800d41ac20ca64adf2b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-28 01:03:04 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -12,21 +12,19 @@ const MovieList = ({ trendingMovies }) => {
   const location = useLocation();
 
   return (
-    <main>
-      <StyledSection>
-        <SectionTitle>Trending today</SectionTitle>
-        <List>
-          {trendingMovies &&
-            trendingMovies.map(({ id, title }) => (
-              <ListItem key={id}>
-                <StyledLink to={`/movies/${id}`} state={{ from: location }}>
-                  {title}
-                </StyledLink>
-              </ListItem>
-            ))}
-        </List>
-      </StyledSection>
-    </main>
+    <StyledSection>
+      <SectionTitle>Trending today</SectionTitle>
+      <List>
+        {trendingMovies &&
+          trendingMovies.map(({ id, title }) => (
+            <ListItem key={id}>
+              <StyledLink to={`/movies/${id}`} state={{ from: location }}>
+                {title}
+              </StyledLink>
+            </ListItem>
+          ))}
+      </List>
+    </StyledSection>
   );
 };
 
